0b6eb62c9f67e4424c3d27676be0a00f675f596d
fengluhome <fengluhome@163.com>
2015-06-18 22:20:56 +0800

    s

--- useStrict/useStrict/strict.js
+++ useStrict/useStrict/strict.js
@@ -4,7 +4,7 @@
  * 严禁模式下赋值给未声明的变量会报错
  */
 function sum(numbers) {
-  //  "use strict";
+    //  "use strict";
     sm = 0;
     for (var i = 0; i < numbers.length; i++) {
         sm += numbers[i];
@@ -22,16 +22,28 @@ function es3vsEs5(a, a, a) {
     //"use strict";
     console.log(arguments);
     return a;
-    
+
 }
 
 es3vsEs5(2, 3, 6);
-
+/*
+ *ES3中，arrgument 对象和函数的形参共享一个动态的关系
+ */
 function swithcArgs(a, b) {
-    "use strict";
+    //  "use strict";
     var c = b;
     b = a;
     a = c;
     return [].slice.call(arguments);
 }
-swithcArgs(2, 3);
+console.log(swithcArgs(2, 3));
+
+/*
+* ES5中删除未成功 抛出异常  ES3中着返回boolean  
+*/
+function deleteStatic(a, b) {
+    "use strict";
+    var a = { val: 3 };
+    delete a.val;
+}
+ 
